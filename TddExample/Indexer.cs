af7723fea586a7f89c2f5be6e4b086f665978292
DmitryLaptev <60900680+DmitryLaptev@users.noreply.github.com>
2020-04-09 01:19:53 +0300

    Add files via upload

---
+++ TddExample/Indexer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace TddExample
+{
+    public class Indexer
+    {
+        private double[] array1;
+        private int beg;
+        private int end;
+        public int getLength
+        {
+            get => end - beg + 1;
+        }
+        /*private void setLength
+        {
+            set => array.Length = value;
+        }*/
+        public Indexer(double[] array, int beg, int end)
+        {
+            if (beg > end || end > array.Length || beg < 0) throw new ArgumentException();
+            else
+            {
+                array1 = array;
+                this.beg = beg;
+                this.end = end;
+            }
+        } 
+        public double this[int ind]
+        {
+            get
+            {
+                if (!IndexCheck(ind)) throw new IndexOutOfRangeException();
+                else return array1[ind + beg];
+            }
+            set
+            {
+                if (!IndexCheck(ind)) throw new IndexOutOfRangeException();
+                array1[ind + beg] = value; 
+            }
+        }
+        bool IndexCheck(int ind)
+        {
+            if (ind >= array1.Length || ind<0 || ind+beg>=array1.Length) throw new ArgumentException();
+            return true;
+        }
+    }
+}
---
+++ TddExample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TddExample;
+namespace TddExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*double[] array = { 13.5, 10, 178.2};
+            Indexer indexer = new Indexer(array, 1, 10);
+            for (int i = 0; i < array.Length; i++) Console.WriteLine(indexer[i]);
+            Console.WriteLine();*/ // example for tests
+        }
+    }
+}
